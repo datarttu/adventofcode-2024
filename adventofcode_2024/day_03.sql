2b22e61e2cf731cf7ce7720acdf6856acc727cde
datarttu <arttu.kosonen@gmail.com>
2024-12-03 08:39:12 +0200

    Day 1 part 1/2 SQL works with test data

---
+++ adventofcode_2024/day_03.sql
@@ -0,0 +1,16 @@
+with extracted_operations as (
+    select section,
+        unnest(regexp_extract_all(section, 'mul\(\d+,\d+\)')) as operation
+    from read_csv(
+            'inputs/test_03.txt',
+            header = false,
+            columns = { 'section': 'VARCHAR' }
+        )
+)
+select sum(
+        list_reduce(
+            regexp_extract_all(operation, '\d+'),
+            (x, y)->x::integer * y::integer
+        )::integer
+    ) as result
+from extracted_operations;
